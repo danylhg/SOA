973757e499747700994f886aac60509e883afa2d
danylhg <200920043@ucc.mx>
2025-11-03 00:46:17 -0600

    Delete apartamentos directory

--- apartamentos/src/main/java/com/blue/apartamentos/repositories/IClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.blue.apartamentos.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.blue.apartamentos.models.ClienteModel;
-
-public interface IClienteRepository extends JpaRepository<ClienteModel, Long> { }
